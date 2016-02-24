0cec241a804854cd941d94339edee746ef4db59f
Paige Hetherington <herbmama@gmail.com>
2016-02-24 09:54:39 -0500

    Add external dr for CSS

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -13,6 +13,7 @@ public class Main {
     static HashMap<String, User> users = new HashMap<>();
 
     public static void main(String[] args) {
+        Spark.externalStaticFileLocation("public");
     Spark.init();
     Spark.get(
             "/",
